b3dfc30598df05bfee2862a5091809d3589eed5c
Adair Juneo <adair_juneo@hotmail.com>
2022-08-25 13:55:06 -0300

    DefaultLayout criado e aplicado na página de payment_methods.

--- src/components/pages/records/payment_methods/index.tsx
+++ src/components/pages/records/payment_methods/index.tsx
@@ -1,13 +1,11 @@
 // Imports React
-import React, { useContext } from 'react'
+import React, { Fragment, useContext } from 'react'
 
 // Chakra Imports
-import { Box, Flex, Text, Button, HStack, Spinner } from '@chakra-ui/react'
+import { Box, Button, HStack, Spinner, Text } from '@chakra-ui/react'
 
 // Components Imports
-import { Header, SideBarNavigation } from '../../../common'
 import { TablePaymentMethods } from '../../../pages/records/payment_methods/Table'
-import { DrawerPaymentMethods } from '../../../pages/records/payment_methods/Drawer'
 
 // Contexts Imports
 import { PaymentMethodsPageContext } from '../../../../contexts/pages/records'
@@ -27,57 +25,38 @@ export const Container: React.FC = () => {
   const { isFetching, isLoading } = paymentMethods
 
   return (
-    <Flex width={'calc(100vw - 1px)'} height='auto' flexDirection='column'>
-      <Header />
-      <DrawerPaymentMethods />
-
-      <Flex
-        gap='12'
-        width='100%'
-        marginTop='10'
+    <Fragment>
+      <HStack
+        spacing='4'
+        alignItems='center'
         marginBottom='6'
-        marginX='auto'
-        maxWidth={1480}
-        paddingX='6'
+        flexDirection='row'
+        justifyContent='space-between'
       >
-        <Flex flexDirection='column'>
-          <SideBarNavigation />
-        </Flex>
+        <HStack spacing='4' alignItems='center'>
+          <Text as='h1' fontSize='3xl' fontWeight='extrabold' lineHeight='1'>
+            Métodos de Pagamento
+          </Text>
+          {isFetching && !isLoading && (
+            <Spinner color='green.500' size='md' thickness='4px' speed='0.5s' />
+          )}
+        </HStack>
 
-        <Flex flexDirection='column' width='100%' flex='1'>
-          <HStack
-            spacing='4'
-            alignItems='center'
-            marginBottom='6'
-            flexDirection='row'
-            justifyContent='space-between'
+        <HStack spacing='4'>
+          <Button
+            type='button'
+            colorScheme='green'
+            leftIcon={<FiCreditCard fontSize='18' />}
+            onClick={onOpen}
           >
-            <HStack spacing='4' alignItems='center'>
-              <Text as='h1' fontSize='3xl' fontWeight='extrabold' lineHeight='1'>
-                Métodos de Pagamento
-              </Text>
-              {isFetching && !isLoading && (
-                <Spinner color='green.500' size='md' thickness='4px' speed='0.5s' />
-              )}
-            </HStack>
+            Novo método de pagamento
+          </Button>
+        </HStack>
+      </HStack>
 
-            <HStack spacing='4'>
-              <Button
-                type='button'
-                colorScheme='green'
-                leftIcon={<FiCreditCard fontSize='18' />}
-                onClick={onOpen}
-              >
-                Novo método de pagamento
-              </Button>
-            </HStack>
-          </HStack>
-
-          <Box as='div' paddingX='8'>
-            <TablePaymentMethods />
-          </Box>
-        </Flex>
-      </Flex>
-    </Flex>
+      <Box as='div' paddingX='8'>
+        <TablePaymentMethods />
+      </Box>
+    </Fragment>
   )
 }
--- src/pages/records/payment_methods.tsx
+++ src/pages/records/payment_methods.tsx
@@ -1,27 +1,33 @@
 // Imports React
-import React from 'react'
 
 // Imports Next
-import NextHead from 'next/head'
-import { getServerSession } from 'next-auth'
 import { GetServerSideProps, NextPage } from 'next'
+import { getServerSession } from 'next-auth'
+import dynamic from 'next/dynamic'
+import NextHead from 'next/head'
 import { authOptions } from '../api/auth/[...nextauth]'
 
 // Components Imports
 import { Container } from '../../components/pages/records/payment_methods'
+const DrawerPaymentMethods = dynamic(
+  () => import('../../components/pages/records/payment_methods/Drawer'),
+)
 
 // Contexts Imports
 import { PaymentMethodsPageContextProvider } from '../../contexts/pages/records'
+import DefaultLayout from '../../layouts/defaultLayout'
 
 const PaymentMethodPage: NextPage = () => {
   return (
-    <PaymentMethodsPageContextProvider>
-      <NextHead>
-        <title>my.finance$ | Métodos de Pagamento</title>
-      </NextHead>
-
-      <Container />
-    </PaymentMethodsPageContextProvider>
+    <DefaultLayout>
+      <PaymentMethodsPageContextProvider>
+        <NextHead>
+          <title>my.finance$ | Métodos de Pagamento</title>
+        </NextHead>
+        <DrawerPaymentMethods />
+        <Container />
+      </PaymentMethodsPageContextProvider>
+    </DefaultLayout>
   )
 }
 
